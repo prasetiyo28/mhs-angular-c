3df7673643b069747bff5145f9331bfb0d6b4803
unknown <bayu28.bap@gmail.com>
2017-11-01 17:33:49 +0700

    Original

---
+++ src/main/java/kimball/cho/mhsangularc/MhsAngularCApplication.java
@@ -0,0 +1,12 @@
+package kimball.cho.mhsangularc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MhsAngularCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MhsAngularCApplication.class, args);
+	}
+}
